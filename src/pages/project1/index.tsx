42384a1c745ed8dcea3adc4cb1c0a73fdb2975db
Isaac Correia Gonçalves <isaac.correia.2406@gmail.com>
2022-12-13 11:25:02 -0300

    -

--- src/pages/project1/index.tsx
+++ src/pages/project1/index.tsx
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 import styles from "./styles.module.scss";
 import randomcolor from "randomcolor";
@@ -31,39 +30,51 @@ function App() {
   };
 
   function createCircle(event) {
-    console.log("Clicou");
-    const [X, Y] = getClickPosition(event);
-    console.log(Y);
-    console.log(X);
-    let newCircle = (
-      <circle
-        className="circle"
-        key={Math.random()}
-        cx={X}
-        cy={Y}
-        r="10"
-        stroke="black"
-        strokeWidth="1"
-        fill={randomcolor()}
-      />
-    );
-    let allCircles = [...circles, newCircle];
-    console.log("allCircles", allCircles);
-    setCircles(allCircles);
+    //repeats the creation of circles when holding the mouse button
+    for (let i = 0; i < 100; i++) {
+
+      if(event.keyup) return;
+      console.log("Clicou");
+      const [X, Y] = getClickPosition(event);
+      console.log(Y);
+      console.log(X);
+      let newCircle = (
+        <circle
+          className="circle"
+          key={Math.random()}
+          cx={X}
+          cy={Y}
+          r="10"
+          stroke="black"
+          strokeWidth="1"
+          fill={randomcolor()}
+        />
+      );
+      let allCircles = [...circles, newCircle];
+      console.log("allCircles", allCircles);
+      setCircles(allCircles);
+      setTimeout(() => console.log("timeout"), 1000);
+    }
   }
 
   return (
     <div className={styles.App}>
       {" "}
-      <h1 className={styles.h1}>Clique em qualquer lugar para criar um circulo</h1>
+      <h1 className={styles.h1}>
+        Clique em qualquer lugar para criar um circulo
+      </h1>
       <div onClick={createCircle} className={styles.circle}>
         <svg width="100%" height="100%">
           {circles}
         </svg>
-      <div className={styles.buttons}>
-      <button className={styles.button} onClick={deleteCircle}>Undo</button>
-      <button className={styles.button} onClick={restoreCircle}>Redo</button>
       </div>
+      <div className={styles.buttons}>
+        <button className={styles.button} onClick={deleteCircle}>
+          Undo
+        </button>
+        <button className={styles.button} onClick={restoreCircle}>
+          Redo
+        </button>
       </div>
     </div>
   );
