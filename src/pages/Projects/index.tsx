1e23e11a20645fa0669075bc8e71c225d0ee40f6
Isaac Correia Gonçalves <82903174+isaac-goncalves@users.noreply.github.com>
2022-12-13 15:22:45 -0300

    -

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -7,22 +7,26 @@ import styles from './styles.module.scss'
 
 import project1 from 'public/project1.png'
 
+import project2 from 'public/project2.png'
+
 export default function index() {
     return (
         <div className={styles.container}>
-        <h1 className={styles.title}>my projects</h1>
+            <h1 className={styles.title}>my projects</h1>
             <div className={styles.projectsContainer}>
                 <a href="./project1" className={styles.project} >
-                    <Image 
-                    src={project1}
+                    <Image
+                        src={project1}
                     />
                     <h1>Circle Test</h1>
                     <p>Projeto desenvolvido em React para demonstrar o funcionamento de um botão de <strong>UNDO</strong> e um de <strong>REDO</strong></p>
                 </a>
                 <div className={styles.project}>
-                    <img src="https://img.freepik.com/premium-photo/abstract-technology-binary-code-background_34629-592.jpg" alt="" />
-                    <h1>Project Name</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore qui consequatur dignissimos culpa est suscipit dolore ad fuga ipsam eveniet voluptas, ullam temporibus cupiditate deleniti unde at placeat numquam! Magni.</p>
+                    <Image
+                        src={project2}
+                    />
+                    <h1>Next.js Pokedex</h1>
+                    <p>Projeto desenvolvido para testar o consumo de API utilizando <strong>Next.js.</strong></p>
                 </div>
                 <div className={styles.project}>
                     <img src="https://img.freepik.com/premium-photo/abstract-technology-binary-code-background_34629-592.jpg" alt="" />
