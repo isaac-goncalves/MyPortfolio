c503b67803a5ccb06493ad602f84804c7f3d1378
Isaac Correia Gonçalves <82903174+isaac-goncalves@users.noreply.github.com>
2022-12-13 17:36:01 -0300

    added language badges

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -20,34 +20,39 @@ export default function index() {
                     />
                     <h1>Circle Test</h1>
                     <p>Projeto desenvolvido em React para demonstrar o funcionamento de um botão de <strong>UNDO</strong> e um de <strong>REDO</strong></p>
+                    <img className={styles.badge} src="https://img.shields.io/badge/-ReactJs-61DAFB?logo=react&logoColor=white&style=flat" />
                 </a>
-                <div className={styles.project}>
+                <a className={styles.project}>
                     <Image
+                        height={500}
                         src={project2}
                     />
                     <h1>Next.js Pokedex</h1>
-                    <p>Projeto desenvolvido para testar o consumo de API utilizando <strong>Next.js.</strong></p>
-                </div>
-                <div className={styles.project}>
+                    <p>Projeto desenvolvido para testar o consumo de API utilizando <strong>Next.js.</strong>
+                    </p>
+                    <img className={styles.badge} src="https://img.shields.io/badge/-ReactJs-61DAFB?logo=react&logoColor=white&style=flat" />
+                </a>
+                <a className={styles.project}>
                     <img src="https://img.freepik.com/premium-photo/abstract-technology-binary-code-background_34629-592.jpg" alt="" />
                     <h1>Project Name</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore qui consequatur dignissimos culpa est suscipit dolore ad fuga ipsam eveniet voluptas, ullam temporibus cupiditate deleniti unde at placeat numquam! Magni.</p>
-                </div>
-                <div className={styles.project}>
+                    <img className={styles.badge} src="https://img.shields.io/badge/-ReactJs-61DAFB?logo=react&logoColor=white&style=flat" />
+                </a>
+                <a className={styles.project}>
                     <img src="https://img.freepik.com/premium-photo/abstract-technology-binary-code-background_34629-592.jpg" alt="" />
                     <h1>Project Name</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore qui consequatur dignissimos culpa est suscipit dolore ad fuga ipsam eveniet voluptas, ullam temporibus cupiditate deleniti unde at placeat numquam! Magni.</p>
-                </div>
-                <div className={styles.project}>
+                </a>
+                <a className={styles.project}>
                     <img src="https://img.freepik.com/premium-photo/abstract-technology-binary-code-background_34629-592.jpg" alt="" />
                     <h1>Project Name</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore qui consequatur dignissimos culpa est suscipit dolore ad fuga ipsam eveniet voluptas, ullam temporibus cupiditate deleniti unde at placeat numquam! Magni.</p>
-                </div>
-                <div className={styles.project}>
+                </a>
+                <a className={styles.project}>
                     <img src="https://img.freepik.com/premium-photo/abstract-technology-binary-code-background_34629-592.jpg" alt="" />
                     <h1>Project Name</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore qui consequatur dignissimos culpa est suscipit dolore ad fuga ipsam eveniet voluptas, ullam temporibus cupiditate deleniti unde at placeat numquam! Magni.</p>
-                </div>
+                </a>
             </div>
         </div>
     )
