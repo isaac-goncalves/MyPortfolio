75318cf066f69976de694972afefc937e05b8748
Isaac Correia Gonçalves <82903174+isaac-goncalves@users.noreply.github.com>
2022-12-14 12:12:46 -0300

    added badges and changed card style

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -18,40 +18,77 @@ export default function index() {
                     <Image
                         src={project1}
                     />
-                    <h1>Circle Test</h1>
+                    <h1>Circles</h1>
                     <p>Projeto desenvolvido em React para demonstrar o funcionamento de um botão de <strong>UNDO</strong> e um de <strong>REDO</strong></p>
-                    <img className={styles.badge} src="https://img.shields.io/badge/-ReactJs-61DAFB?logo=react&logoColor=white&style=flat" />
+                    <div className={styles.badges_container} >
+                        <img className={styles.badge}
+                            src="https://img.shields.io/badge/-ReactJs-61DAFB?logo=react&logoColor=white&style=for-the-badge"
+                        />
+                        <img className={styles.badge}
+                            src="https://img.shields.io/badge/-Saas-Bf4080?logo=sass&logoColor=white&style=for-the-badge"
+                        />
+                    </div>
+
                 </a>
                 <a className={styles.project}>
                     <Image
                         height={500}
                         src={project2}
                     />
-                    <h1>Next.js Pokedex</h1>
+                    <h1>Pokedex</h1>
                     <p>Projeto desenvolvido para testar o consumo de API utilizando <strong>Next.js.</strong>
                     </p>
-                    <img className={styles.badge} src="https://img.shields.io/badge/-ReactJs-61DAFB?logo=react&logoColor=white&style=flat" />
+                    <div className={styles.badges_container} >
+                        <img className={styles.badge}
+                            src="https://img.shields.io/badge/-ReactJs-61DAFB?logo=react&logoColor=white&style=for-the-badge"
+                        />
+                        <img className={styles.badge}
+                            src="https://img.shields.io/badge/-Axios-E6E6FA?logo=axios&logoColor=black&style=for-the-badge"
+                        />
+                        <img className={styles.badge}
+                            src="https://img.shields.io/badge/-Saas-Bf4080?logo=sass&logoColor=white&style=for-the-badge"
+                        />
+                    </div>
                 </a>
                 <a className={styles.project}>
-                    <img src="https://img.freepik.com/premium-photo/abstract-technology-binary-code-background_34629-592.jpg" alt="" />
+                    <Image
+                        height={500}
+                        src={project2}
+                    />
                     <h1>Project Name</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore qui consequatur dignissimos culpa est suscipit dolore ad fuga ipsam eveniet voluptas, ullam temporibus cupiditate deleniti unde at placeat numquam! Magni.</p>
-                    <img className={styles.badge} src="https://img.shields.io/badge/-ReactJs-61DAFB?logo=react&logoColor=white&style=flat" />
+                    <p>Lorem us cupiditate deleniti unde at placeat numquam! Magni.</p>
+                    <div className={styles.badges_container} >
+                        <img className={styles.badge}
+                            src="https://img.shields.io/badge/-ReactJs-61DAFB?logo=react&logoColor=white&style=for-the-badge"
+                        />
+                        <img className={styles.badge}
+                            src="https://img.shields.io/badge/-Axios-E6E6FA?logo=axios&logoColor=black&style=for-the-badge"
+                        />
+                    </div>
                 </a>
                 <a className={styles.project}>
-                    <img src="https://img.freepik.com/premium-photo/abstract-technology-binary-code-background_34629-592.jpg" alt="" />
+                    <Image
+                        height={500}
+                        src={project2}
+                    />
                     <h1>Project Name</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore qui consequatur dignissimos culpa est suscipit dolore ad fuga ipsam eveniet voluptas, ullam temporibus cupiditate deleniti unde at placeat numquam! Magni.</p>
+                    <p>Lorem us cupiditate deleniti unde at placeat numquam! Magni.</p>
                 </a>
                 <a className={styles.project}>
-                    <img src="https://img.freepik.com/premium-photo/abstract-technology-binary-code-background_34629-592.jpg" alt="" />
+                    <Image
+                        height={500}
+                        src={project2}
+                    />
                     <h1>Project Name</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore qui consequatur dignissimos culpa est suscipit dolore ad fuga ipsam eveniet voluptas, ullam temporibus cupiditate deleniti unde at placeat numquam! Magni.</p>
+                    <p>Lorem us cupiditate deleniti unde at placeat numquam! Magni.</p>
                 </a>
                 <a className={styles.project}>
-                    <img src="https://img.freepik.com/premium-photo/abstract-technology-binary-code-background_34629-592.jpg" alt="" />
+                    <Image
+                        height={500}
+                        src={project2}
+                    />
                     <h1>Project Name</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore qui consequatur dignissimos culpa est suscipit dolore ad fuga ipsam eveniet voluptas, ullam temporibus cupiditate deleniti unde at placeat numquam! Magni.</p>
+                    <p>Lorem us cupiditate deleniti unde at placeat numquam! Magni.</p>
                 </a>
             </div>
         </div>
