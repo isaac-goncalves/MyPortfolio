fc7f5c430607a0872bb9f06508e6026554545d2e
Isaac Correia Gonçalves <82903174+isaac-goncalves@users.noreply.github.com>
2022-12-20 08:46:40 -0300

    -

--- src/pages/project2/index.jsx
+++ src/pages/project2/index.jsx
@@ -18,6 +18,7 @@ const capitalizeFirstLetter = (string) => {
 export default function Project2() {
   const [pokemons, setPokemons] = useState([]);
   const test = ["teste", "teste2", "teste3"];
+  const [search, setSearch] = useState("");
   //   const [teste, setTeste] = useState("3");
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
@@ -32,29 +33,32 @@ export default function Project2() {
     <>
       <div className={styles.container}>
         <h1 className={styles.title}>Pokedex</h1>
+        <input onChange={setSearch} type="text" />
+        <button>Search</button>
         <div className={styles.cardContainer}>
-          {pokemons.map((pokemon) => {
-            return (
-              <div className={styles.card} key={pokemon.name}>
-                <h1 className={styles.cardTitle}>
-                  {capitalizeFirstLetter(pokemon.name)}
-                </h1>
-                <div className={styles.image}>
-                  <Image
-                    width={200}
-                    height={200}
-                    src={getPokemonImage(
-                      pokemon.url.split("/")[pokemon.url.split("/").length - 2]
-                    )}
-                    alt={pokemon.name}
-                  />
+          {
+            pokemons.map((pokemon) => {
+              return (
+                <div className={styles.card} key={pokemon.name}>
+                  <h1 className={styles.cardTitle}>
+                    {capitalizeFirstLetter(pokemon.name)}
+                  </h1>
+                  <div className={styles.image}>
+                    <Image
+                      width={200}
+                      height={200}
+                      src={getPokemonImage(
+                        pokemon.url.split("/")[pokemon.url.split("/").length - 2]
+                      )}
+                      alt={pokemon.name}
+                    />
+                  </div>
+                  <div className={styles.roudedCard}>
+                    <DetailsPart url={pokemon.url} />
+                  </div>
                 </div>
-                <div className={styles.roudedCard}>
-                  <DetailsPart url={pokemon.url} />
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
         </div>
       </div>
     </>
