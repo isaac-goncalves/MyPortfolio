e488e7dda29986d1f315f67fb38e651a8090513f
Isaac Correia Gonçalves <82903174+isaac-goncalves@users.noreply.github.com>
2022-12-20 17:17:55 -0300

    -

--- src/pages/project2/index.jsx
+++ src/pages/project2/index.jsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 
 import Image from "next/image";
-import internal from "stream";
 
 import DetailsPart from "./components/Details";
 
