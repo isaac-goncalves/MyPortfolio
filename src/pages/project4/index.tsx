e1b4e4afb9542517adfb03bbe1172d217e01e653
Isaac Correia Gonçalves <isaac.correia.2406@gmail.com>
2023-02-14 13:27:22 -0300

    added styling and logo

--- src/pages/project4/index.tsx
+++ src/pages/project4/index.tsx
@@ -1,11 +1,15 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React, { useState } from 'react'
 import styles from "./styles.module.scss";
+import { AiOutlineSend } from 'react-icons/ai';
+import { IoIosArrowBack } from 'react-icons/Io';
+import Image from 'next/image';
 
 
 function index() {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const [messages, setMessages] = useState([])
-  const [input, setInput] = useState([])
+  const [input, setInput] = useState('')
 
   const handleChange = e => {
     setInput(e.target.value);
@@ -13,8 +17,7 @@ function index() {
 
   function handleNewMessage(e) {
 
-
-      e.preventDefault();
+    e.preventDefault();
 
     console.log(input)
 
@@ -32,33 +35,68 @@ function index() {
   return (
     <div className={styles.container}>
       <div>
-        <div>GTP chat</div>
-        <div className={styles.messages_contaniner}>
-          <div className={styles.human_message}>Bom dia chat</div>
-          <div className={styles.ai_message}>Boa tarde humano</div>
+        <div className={styles.messages_container}>
+          <div className={styles.header}>
+            <div className={styles.goback}>
+            <IoIosArrowBack size={30}  />
+            </div>
+            <div className={styles.avatar}>
+            <Image
+              src="/showdomilhao-logo.png"
+              alt="Picture of the author" 
+              width={40}
+              height={40}
+            />
+          </div>
+          <p>
+            GTP chat
+          </p>
+        </div>
+        <div className={styles.messages_wrapper}>
+          <div className={styles.human_message}>
+            <div >Bom dia chat</div>
+            <div className={styles.message_timestamp}>11:34:45</div>
+          </div>
+          <div className={styles.ai_message}>
+            <div >Boa tarde humano</div>
+            <div className={styles.message_timestamp}>11:34:45</div>
+          </div>
           {
             messages.map((message) => {
               return (<>
-                <div key={message.id} className={styles.human_message}>{message.message}</div>
-                <div className={styles.human_message_timestamp}>{message.time.toLocaleTimeString()}</div>
+                <div className={styles.human_message}>
+                  <div key={message.id} >{message.message}</div>
+                  <div className={styles.message_timestamp}>{message.time.toLocaleTimeString()}</div>
+                </div>
               </>
               )
             })
           }
         </div>
-        <div>
-          <form onSubmit={handleNewMessage}>
+        <form onSubmit={handleNewMessage}>
+          <div className={styles.form_container}>
             <input
               value={input}
               onChange={handleChange}
               name='text'
               className={styles.input}
               placeholder="Digite sua mensagem"
-            ></input>
-          </form>
-        </div>
+            >
+            </input>
+            <span
+              onClick={handleNewMessage}
+            >
+              <AiOutlineSend
+                size={30}
+              />
+            </span>
+          </div>
+        </form>
+      </div>
+      <div>
       </div>
     </div>
+    </div >
   )
 }
 
